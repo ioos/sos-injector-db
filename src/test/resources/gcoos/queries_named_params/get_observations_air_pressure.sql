08ed29016104064cd17d1b1de234d1fc297fb7ee
Shane StClair <shane@axiomalaska.com>
2014-10-02 00:07:47 -0700

    Fix gcoos test

--- src/test/resources/gcoos/queries_named_params/get_observations_air_pressure.sql
+++ src/test/resources/gcoos/queries_named_params/get_observations_air_pressure.sql
@@ -10,7 +10,7 @@ SELECT
 FROM airPressure a
 JOIN sensor b
  ON a.sensorId = b.rowid
-WHERE b.platformId = :station_database_idstation_database_id
+WHERE b.platformId = :station_database_id
 AND :sensor_database_id IS NOT NULL --do nothing with the sensor_database_id
 AND a.observationDate > strftime('%Y-%m-%dT%H:%M:%SZ', :start_date)
 AND a.barometric IS NOT NULL
